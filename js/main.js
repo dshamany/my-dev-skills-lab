20f9a0d1ef461bfb01dae4fa4cc58d9b1483a588
Daniel Shamany <dshamany@gmail.com>
2019-11-04 14:23:52 -0800

    Persistance should work accross sessions now

--- js/main.js
+++ js/main.js
@@ -2,12 +2,23 @@ let $container = $('.container');
 let $input = $('input');
 let $addBtn = $('#btnAdd');
 
+let storage = sessionStorage;
+
+function init() {
+    for (let i = storage.length; i >= 0; --i) {
+        $container.append(storage[storage.key(i)]);
+    }
+}
+
+init();
+
 // JQuery version
-function createItem(){
+function createItem() {
+    let id = storage.length;
     if ($input.val() === '') return;
     let $item = $(`
-        <div id='skill-item'>
-            <button id="btnDelete" class="btn btn-danger pull-left">
+        <div class="skill-item">
+            <button id="${id}" class="btn btn-danger pull-left">
             X
             </button>
             <span id="skill-value">
@@ -15,16 +26,18 @@ function createItem(){
             </span>
         </div>
     `);
+    storage.setItem(id, $item[0].outerHTML);
     $container.append($item);
     $input.val('');
     $input.focus();
 }
 
-function handleDelete(evt){
-    $(this).closest('#skill-item').fadeOut(300, () => {
-        $(this).closest("#skill-item").remove();
+function handleDelete(evt) {
+    $(this).closest('.skill-item').fadeOut(300, () => {
+        $(this).closest(".skill-item").remove();
+        storage.removeItem($(this)[0].id);
     });
 }
-
+ 
 $addBtn.on('click', createItem);
 $container.on('click', 'button', handleDelete);
